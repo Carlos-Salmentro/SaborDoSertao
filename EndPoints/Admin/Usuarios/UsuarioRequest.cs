9e434a6a10af43809ff8605d48d72d635e6580fd
CarlosSalmentro <carlos.salmentro@gmail.com>
2023-04-05 20:02:26 -0300

    Add - UsuarioRequest

---
+++ EndPoints/Admin/Usuarios/UsuarioRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace SaborDoSertão.EndPoints.Admin.Usuarios
+{
+    public class UsuarioRequest
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string CPF { get; set; }
+        public string Password { get; set; }
+        
+    }
+}
