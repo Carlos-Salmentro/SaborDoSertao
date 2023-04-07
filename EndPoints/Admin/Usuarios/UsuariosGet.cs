9b6860612d6d55d48594a68f22c4cf32b49fafb7
CarlosSalmentro <carlos.salmentro@gmail.com>
2023-04-07 00:00:22 -0300

    Add - AdminUsuarioGet

---
+++ EndPoints/Admin/Usuarios/UsuariosGet.cs
@@ -0,0 +1,14 @@
+﻿namespace SaborDoSertão.EndPoints.Admin.Usuarios
+{
+    public class UsuariosGet
+    {
+        public static string Template => "/admin/usuarios";
+        public static string[] Methods => new[] {HttpMethods.Get};
+        public static Delegate Handler => Action;
+
+        public static IResult Action(string id)
+        {
+            return Results.Ok();
+        }
+    }
+}
