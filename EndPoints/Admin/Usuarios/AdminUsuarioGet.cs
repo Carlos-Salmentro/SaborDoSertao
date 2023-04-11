e16d5f176cdbd233927cf807a46d87a964de35de
CarlosSalmentro <carlos.salmentro@gmail.com>
2023-04-11 10:29:18 -0300

    Add - EndPoint AdminUsuarioGet(ById)

---
+++ EndPoints/Admin/Usuarios/AdminUsuarioGet.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SaborDoSertão.EndPoints.Admin.Usuarios
+{
+    public class AdminUsuarioGet
+    {
+        public static string Template => "/Admin/Usuarios/{usuarioId}";
+        public static string[] Methods = new string[] {HttpMethods.Get.ToString()};
+        public static Delegate Handler = Action;
+
+        public static IResult Action([FromRoute] string usuarioId, [FromServices] UserManager<IdentityUser> userManager)
+        {
+            Task user = userManager.FindByIdAsync(usuarioId);
+
+            return Results.Ok(user);
+        }
+    }
+}
