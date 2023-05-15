48ba58e68b6ff0bf87e5dfe89f8ccfa9e11c0ac4
CarlosSalmentro <carlos.salmentro@gmail.com>
2023-05-15 12:18:52 -0300

    Add - JWT information at appsettings

--- EndPoints/LoginToken/LoginTokenPost.cs
+++ EndPoints/LoginToken/LoginTokenPost.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Routing.Template;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Routing.Template;
 
 namespace SaborDoSertão.EndPoints.LoginToken
 {
@@ -8,6 +9,10 @@ namespace SaborDoSertão.EndPoints.LoginToken
         public static string[] Methods => new[] {HttpMethods.Post.ToString()};
         public static Delegate Handler = Action;
 
-        public static IResult Action()
+        public static IResult Action(LoginRequest loginRequest, UserManager<IdentityUser> userManager)
+        {
+
+            return Results.Ok();
+        }
     }
 }
