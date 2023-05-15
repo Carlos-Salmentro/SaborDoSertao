e9dc10af03fa59fa4d8b2037c1a9b00e5a80ca4a
CarlosSalmentro <carlos.salmentro@gmail.com>
2023-05-15 11:12:58 -0300

    Refactor - LoginTokenPost Template to /login

--- EndPoints/LoginToken/LoginTokenPost.cs
+++ EndPoints/LoginToken/LoginTokenPost.cs
@@ -1,6 +1,13 @@
-﻿namespace SaborDoSertão.EndPoints.LoginToken
+﻿using Microsoft.AspNetCore.Routing.Template;
+
+namespace SaborDoSertão.EndPoints.LoginToken
 {
     public class LoginTokenPost
     {
+        public static string Template => "/login";
+        public static string[] Methods => new[] {HttpMethods.Post.ToString()};
+        public static Delegate Handler = Action;
+
+        public static IResult Action()
     }
 }
